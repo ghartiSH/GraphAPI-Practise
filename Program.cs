6d431900addbb2ae0ab06e0106b1032d0959865b
Bharat Gharti <bharatgharti1963@gmail.com>
2022-12-08 16:51:15 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using GraphTutorial;
+
+Console.WriteLine(".NET Graph Tutorial\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+
+int choice = -1;
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display Welcome Message");
+    Console.WriteLine("2. Display all mails");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch (choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            await GreetUserAsync();
+            break;
+        case 2:
+            await ListInboxAsync();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) =>
+        {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        });
+}
+
+async Task GreetUserAsync()
+{
+    try
+    {
+        var user = await GraphHelper.GetUserAsync();
+        var token = await GraphHelper.GetUserTokenAsync();
+        Console.WriteLine($"Hello, {user?.DisplayName}!");
+        Console.WriteLine($"Email: {user?.Mail ?? user?.UserPrincipalName ?? ""}");
+        Console.WriteLine($"Your Token: {token}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user: {ex.Message}");
+    }
+}
+
+async Task ListInboxAsync()
+{
+    try
+    {
+        var messagePage = await GraphHelper.GetInboxAsync();
+
+        // Output each message's details
+        foreach (var message in messagePage.CurrentPage)
+        {
+            Console.WriteLine($"Message: {message.Subject ?? "NO SUBJECT"}");
+            Console.WriteLine($"  From: {message.From?.EmailAddress?.Name}");
+            Console.WriteLine($"  Status: {(message.IsRead!.Value ? "Read" : "Unread")}");
+            Console.WriteLine($"  Received: {message.ReceivedDateTime?.ToLocalTime().ToString()}");
+        }
+
+        // If NextPageRequest is not null, there are more messages
+        // available on the server
+        // Access the next page like:
+        // messagePage.NextPageRequest.GetAsync();
+        var moreAvailable = messagePage.NextPageRequest != null;
+
+        Console.WriteLine($"\nMore messages available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user's inbox: {ex.Message}");
+    }
+}
+
+
+
+
+
+
+
+
